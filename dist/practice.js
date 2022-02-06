28cba10b86962ee554577d19a8c4c6839183bb25
VoidShell <63570013+VoidShell@users.noreply.github.com>
2022-02-06 23:09:39 +0900

    feat: #3 - Define the type in a function.

--- dist/practice.js
+++ dist/practice.js
@@ -1,3 +1,18 @@
 "use strict";
-const message = 'hello world';
-console.log(message);
+// # 1
+// const message: string = 'hello world';
+// console.log(message);
+// # 2
+let count = 0; // 숫자
+count += 1;
+count = '갑자기 분위기 문자열'; // 이러면 에러가 납니다!
+const message = 'hello world'; // 문자열
+const done = true; // 불리언 값
+const numbers = [1, 2, 3]; // 숫자 배열
+const messages = ['hello', 'world']; // 문자열 배열
+messages.push(1); // 숫자 넣으려고 하면.. 안 된다!
+let mightBeUndefined = undefined; // string 일수도 있고 undefined일 수도 있음
+let nullableNumber = null; // number 일수도 있고 null 일 수도 있음
+let color = 'red'; // red, orange, yellow 중 하나임
+color = 'yellow';
+color = 'green'; // 에러 발생
