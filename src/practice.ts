477866a6f94bd1bef880819c05c41277af0cef94
VoidShell <63570013+VoidShell@users.noreply.github.com>
2022-02-06 23:53:10 +0900

    feat: #7 - Use Generics in Functions

--- src/practice.ts
+++ src/practice.ts
@@ -173,29 +173,56 @@
 
 
 // #6: Type Alias 사용하기
-type Person = {
-    name: string;
-    age?: number; // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값이라는 것을 의미합니다.
-};
+// type Person = {
+//     name: string;
+//     age?: number; // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값이라는 것을 의미합니다.
+// };
 
 // & 는 Intersection 으로서 두개 이상의 타입들을 합쳐줍니다.
 // 참고: https://www.typescriptlang.org/docs/handbook/advanced-types.html#intersection-types
-type Developer = Person & {
-    skills: string[];
-};
+// type Developer = Person & {
+//     skills: string[];
+// };
+//
+// const person: Person = {
+//     name: '김사람'
+// };
+//
+// const expert: Developer = {
+//     name: '김개발',
+//     skills: ['javascript', 'react']
+// };
+//
+// type People = Person[]; // Person[] 를 이제 앞으로 People 이라는 타입으로 사용 할 수 있습니다.
+// const people: People = [person, expert];
+//
+// type Color = 'red' | 'orange' | 'yellow';
+// const color: Color = 'red';
+// const colors: Color[] = ['red', 'orange'];
 
-const person: Person = {
-    name: '김사람'
-};
+// #7: 함수에서 Generics 사용하기
+// function merge(a: any, b: any): any {
+//     return {
+//         ...a,
+//         ...b
+//     };
+// }
+//
+// const merged = merge({ foo: 1 }, { bar: 1 });
+// function merge<A, B>(a: A, b: B): A & B {
+//     return {
+//         ...a,
+//         ...b
+//     };
+// }
+//
+// const merged = merge({ foo: 1 }, { bar: 1 });
 
-const expert: Developer = {
-    name: '김개발',
-    skills: ['javascript', 'react']
-};
 
-type People = Person[]; // Person[] 를 이제 앞으로 People 이라는 타입으로 사용 할 수 있습니다.
-const people: People = [person, expert];
+function wrap<T>(param: T) {
+    return {
+        param
+    }
+}
 
-type Color = 'red' | 'orange' | 'yellow';
-const color: Color = 'red';
-const colors: Color[] = ['red', 'orange'];
+const wrapped = wrap(10);
