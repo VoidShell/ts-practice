77d36633ea43b4a1611c69ae52d446f480ef5f41
VoidShell <63570013+VoidShell@users.noreply.github.com>
2022-02-06 23:24:44 +0900

    #5 - Set a general object to interface

--- src/practice.ts
+++ src/practice.ts
@@ -79,46 +79,94 @@
 // });
 
 // Shape 라는 interface 를 선언합니다.
-interface Shape {
-    getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자입니다.
-}
-
-class Circle implements Shape {
-    // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시합니다.
-    constructor(public radius: number) {
-        this.radius = radius;
-    }
-
-    // 너비를 가져오는 함수를 구현합니다.
-    getArea() {
-        return this.radius * this.radius * Math.PI;
-    }
-}
-
-class Rectangle implements Shape {
-    constructor(private width: number, private height: number) {
-        this.width = width;
-        this.height = height;
-    }
-    getArea() {
-        return this.width * this.height;
-    }
-}
-
-const circle = new Circle(5);
-const rectangle = new Rectangle(10, 5);
-
-console.log(circle.radius);
-// console.log(rectangle.width);
-
-const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
-
-shapes.forEach(shape => {
-    console.log(shape.getArea());
-});
+// interface Shape {
+//     getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자입니다.
+// }
+//
+// class Circle implements Shape {
+//     // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시합니다.
+//     constructor(public radius: number) {
+//         this.radius = radius;
+//     }
+//
+//     // 너비를 가져오는 함수를 구현합니다.
+//     getArea() {
+//         return this.radius * this.radius * Math.PI;
+//     }
+// }
+//
+// class Rectangle implements Shape {
+//     constructor(private width: number, private height: number) {
+//         this.width = width;
+//         this.height = height;
+//     }
+//     getArea() {
+//         return this.width * this.height;
+//     }
+// }
+//
+// const circle = new Circle(5);
+// const rectangle = new Rectangle(10, 5);
+//
+// console.log(circle.radius);
+// // console.log(rectangle.width);
+//
+// const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+//
+// shapes.forEach(shape => {
+//     console.log(shape.getArea());
+// });
 
 /**
  * public 으로 선언된 값은 클래스 외부에서 조회 할 수 있으며 private으로 선언된 값은 클래스 내부에서만 조회 할 수 있습니다.
  * 따라서 위 코드에서는 circle 의 radius 값은 클래스 외부에서 조회 할 수 있지만,
  * tsrectangle 의 width 또는 height 값은 클래스 외부에서 조회 할 수 없습니다.
  */
+
+
+// #5: 일반 객체를 interface로 타입 설정하기
+// interface Person {
+//     name: string;
+//     age?: number; // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값이라는 것을 의미합니다.
+// }
+// interface Developer {
+//     name: string;
+//     age?: number;
+//     skills: string[];
+// }
+//
+// const person: Person = {
+//     name: '김사람',
+//     age: 20
+// };
+//
+// const expert: Developer = {
+//     name: '김개발',
+//     skills: ['javascript', 'react']
+// };
+
+/**
+ * 지금 보면 Person 과 Developer 가 형태가 유사하지요?
+ * 이럴 땐 interface 를 선언 할 때 다른 interface 를 extends 키워드를 사용해서 상속받을 수 있습니다.
+ */
+
+
+interface Person {
+    name: string;
+    age?: number; // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값이라는 것을 의미합니다.
+}
+interface Developer extends Person {
+    skills: string[];
+}
+
+const person: Person = {
+    name: '김사람',
+    age: 20
+};
+
+const expert: Developer = {
+    name: '김개발',
+    skills: ['javascript', 'react']
+};
+
+const people: Person[] = [person, expert];
