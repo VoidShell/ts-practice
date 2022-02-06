0daa9d4021fa03f03b69a8e54fc88f18fa2d3179
VoidShell <63570013+VoidShell@users.noreply.github.com>
2022-02-06 23:21:02 +0900

    feat: #4 - Try using the interface

--- dist/practice.js
+++ dist/practice.js
@@ -1,18 +1,39 @@
 "use strict";
-// # 1
+// # 1: 타입스크립트 파일 만들기
 // const message: string = 'hello world';
 // console.log(message);
-// # 2
-let count = 0; // 숫자
-count += 1;
-count = '갑자기 분위기 문자열'; // 이러면 에러가 납니다!
-const message = 'hello world'; // 문자열
-const done = true; // 불리언 값
-const numbers = [1, 2, 3]; // 숫자 배열
-const messages = ['hello', 'world']; // 문자열 배열
-messages.push(1); // 숫자 넣으려고 하면.. 안 된다!
-let mightBeUndefined = undefined; // string 일수도 있고 undefined일 수도 있음
-let nullableNumber = null; // number 일수도 있고 null 일 수도 있음
-let color = 'red'; // red, orange, yellow 중 하나임
-color = 'yellow';
-color = 'green'; // 에러 발생
+class Circle {
+    // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시합니다.
+    constructor(radius) {
+        this.radius = radius;
+        this.radius = radius;
+    }
+    // 너비를 가져오는 함수를 구현합니다.
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+}
+const circle = new Circle(5);
+const rectangle = new Rectangle(10, 5);
+console.log(circle.radius);
+// console.log(rectangle.width);
+const shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach(shape => {
+    console.log(shape.getArea());
+});
+/**
+ * public 으로 선언된 값은 클래스 외부에서 조회 할 수 있으며 private으로 선언된 값은 클래스 내부에서만 조회 할 수 있습니다.
+ * 따라서 위 코드에서는 circle 의 radius 값은 클래스 외부에서 조회 할 수 있지만,
+ * tsrectangle 의 width 또는 height 값은 클래스 외부에서 조회 할 수 없습니다.
+ */ 
--- src/practice.ts
+++ src/practice.ts
@@ -25,16 +25,100 @@
 
 // #3: 함수에서 타입 정의하기
 
-function sum(x: number, y: number): number {
-    return x + y;
+// function sum(x: number, y: number): number {
+//     return x + y;
+// }
+//
+// sum(1, 2);
+//
+//
+// // 숫자 배열의 총합을 구하는 sumArray 함수
+// function sumArray(numbers: number[]): number {
+//     return numbers.reduce((acc, current) => acc + current, 0);
+// }
+//
+// const total = sumArray([1, 2, 3, 4, 5])
+
+// #4: interface 사용해보기
+// Shape 라는 interface 를 선언합니다.
+// interface Shape {
+//     getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자입니다.
+// }
+//
+// class Circle implements Shape {
+//     // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시합니다.
+//
+//     radius: number; // 멤버 변수 radius 값을 설정합니다.
+//
+//     constructor(radius: number) {
+//         this.radius = radius;
+//     }
+//
+//     // 너비를 가져오는 함수를 구현합니다.
+//     getArea() {
+//         return this.radius * this.radius * Math.PI;
+//     }
+// }
+//
+// class Rectangle implements Shape {
+//     width: number;
+//     height: number;
+//     constructor(width: number, height: number) {
+//         this.width = width;
+//         this.height = height;
+//     }
+//     getArea() {
+//         return this.width * this.height;
+//     }
+// }
+//
+// const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+//
+// shapes.forEach(shape => {
+//     console.log(shape.getArea());
+// });
+
+// Shape 라는 interface 를 선언합니다.
+interface Shape {
+    getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자입니다.
 }
 
-sum(1, 2);
+class Circle implements Shape {
+    // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시합니다.
+    constructor(public radius: number) {
+        this.radius = radius;
+    }
 
-
-// 숫자 배열의 총합을 구하는 sumArray 함수
-function sumArray(numbers: number[]): number {
-    return numbers.reduce((acc, current) => acc + current, 0);
+    // 너비를 가져오는 함수를 구현합니다.
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
 }
 
-const total = sumArray([1, 2, 3, 4, 5])
+class Rectangle implements Shape {
+    constructor(private width: number, private height: number) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+const circle = new Circle(5);
+const rectangle = new Rectangle(10, 5);
+
+console.log(circle.radius);
+// console.log(rectangle.width);
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+
+shapes.forEach(shape => {
+    console.log(shape.getArea());
+});
+
+/**
+ * public 으로 선언된 값은 클래스 외부에서 조회 할 수 있으며 private으로 선언된 값은 클래스 내부에서만 조회 할 수 있습니다.
+ * 따라서 위 코드에서는 circle 의 radius 값은 클래스 외부에서 조회 할 수 있지만,
+ * tsrectangle 의 width 또는 height 값은 클래스 외부에서 조회 할 수 없습니다.
+ */
