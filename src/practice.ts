11ad8849b4121394f8e7b1fd1e453a643937ac4e
VoidShell <63570013+VoidShell@users.noreply.github.com>
2022-02-06 23:54:34 +0900

    feat: #8 - Use Generics on the interface

--- src/practice.ts
+++ src/practice.ts
@@ -219,10 +219,20 @@
 // const merged = merge({ foo: 1 }, { bar: 1 });
 
 
-function wrap<T>(param: T) {
-    return {
-        param
-    }
+// function wrap<T>(param: T) {
+//     return {
+//         param
+//     }
+// }
+//
+// const wrapped = wrap(10);
+
+// #8: interface 에서 Generics 사용하기
+interface Items<T> {
+    list: T[];
 }
 
-const wrapped = wrap(10);
+const items: Items<string> = {
+    list: ['a', 'b', 'c']
+};
+
